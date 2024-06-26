80023dcf6d383b5b13bffe29f39c07dd5885b890
Itorophilip1998 <itorophilip1998@gmail.com>
2024-06-26 04:18:41 +0100

    ManageGiftCards

--- src/components/indexPageComponents/ManageGiftCards.jsx
+++ src/components/indexPageComponents/ManageGiftCards.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import CustomImage from '../CustomImage'
 
 function ManageGiftCards() {
   return (
-    <div>
-        
+    <div className="ContainerBox my-0  ManageGiftCards">
+        <div className="row m-0">
+      <div className="col-md-6">
+        <h1>
+        Manage from <br/> anywhere
+
+        </h1>
+        <p>
+        Save your gift cards in your Switchive Wallet to access them easily from your computer or smartphone.
+
+        </p>
+        <button className="btn btn-primary shadow-sm px-3 ">Check Wallet</button>
+
+      </div>
+      <div className="col-md-6">
+      <CustomImage src={'indexPage/manageCards.svg'} />
+
+      </div>
+        </div>
     </div>
   )
 }
