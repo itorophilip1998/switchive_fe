7e19ade27ca7606a104605d78f9553bcb6513937
Itorophilip1998 <itorophilip1998@gmail.com>
2024-06-28 10:33:02 +0100

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <title>Switchive</title>
             <meta name="description" content="Make mobile top-ups, buy gift cards, and pay bills with crypto for more than 14,000 products worldwide in over 170 countries and earn points." />
             <meta name="keywords" content="crypto, mobile top-ups, gift cards, bill payments, crypto payments, global products, crypto points" />
-            <link rel="shortcut icon" href="logo.png" type="image/x-icon" />
+        <link rel="shortcut icon" href="logo.png" type="image/x-icon" />
+      
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous"/>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous" async></script>
       </head>
